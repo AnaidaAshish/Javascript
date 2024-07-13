a06230eea257468ff1041a3297e2c3ef8b1f5719
anaidachan <anaidaambilissery@gmail.com>
2024-07-13 13:11:46 +0530

    updates

--- Day-4/prac.js
+++ Day-4/prac.js
@@ -10,11 +10,11 @@ add("Hello ", "World ", "Have a ", "GoodDay!!");
 
 function checkIsUserQualifiedForDrivingLicence(age) {
     if(age > 18 && age <= 60){
-        console.log("Congratulations You are Qualified for Driving Licence!!");
+        console.log(age,"Congratulations You are Qualified for Driving Licence!!");
     }else if(age <= 17 || age > 60){
-        console.log("Sorry you do not qualify for the Driving Licence");
+        console.log(age, "Sorry you do not qualify for the Driving Licence");
     }else{
-        console.log("You are Eligible for Learning Licence");
+        console.log(age, "You are Eligible for Learning Licence");
     }
 }
-checkIsUserQualifiedForDrivingLicence(17);
+checkIsUserQualifiedForDrivingLicence(100);
